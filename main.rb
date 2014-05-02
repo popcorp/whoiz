8645bbfe0ff592b452b9ba66da4510eafcfc7544
PunKeel <PunKeel@users.noreply.github.com>
2014-05-02 12:15:00 +0200

    Refactor ruby code
    (re)Make DiskFetcher a global method, convert whois() to a function

--- main.rb
+++ main.rb
@@ -7,40 +7,45 @@ require 'yaml'
 require 'simpleidn'
 
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
-set :port => CONFIG['port'], :bind => CONFIG['bind']
-set :cache => CONFIG['cache']
-
+set :port => CONFIG['port'] || 4567
+set :bind => CONFIG['bind'] || "0.0.0.0"
+set :cache => CONFIG['cache'] || 600
+       
 class DiskFetcher
    # Taken from https://developer.yahoo.com/ruby/ruby-cache.html
    def initialize(cache_dir='/tmp')
       @cache_dir = cache_dir
    end
-   def whois(domain, max_age=0)
+   def fetch(domain, max_age=0, func)
       file = Digest::MD5.hexdigest(domain)
       file_path = File.join("", @cache_dir, file)
       if File.exists? file_path
          return File.new(file_path).read if Time.now-File.mtime(file_path)<max_age
       end
-      
-      @whois = Whois.lookup(domain).to_s.force_encoding('utf-8').encode
+         
+      result = func.call(domain)
       File.open(file_path, "w") do |data|
-         data << @whois
+         data << result
       end
-      @whois
+      result
    end
+end   
+   
+$whois = Proc.new do |domain|
+        Whois.lookup(domain).to_s.force_encoding('utf-8').encode
 end
-
+        
 before do
-	response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Origin'] = '*'
 end
-
+                                                                   
 ["/:domain", "/"].each do |path|
   get path do
-  	begin
-		domain = SimpleIDN.to_ascii(params[:domain])
-  		{:result => DiskFetcher.new.whois(domain, settings.cache)}.to_json
-  	rescue Exception => e
-		{:error => e}.to_json
-	end
+        begin
+                domain = SimpleIDN.to_ascii(params[:domain])
+                {:result => DiskFetcher.new.fetch(domain, settings.cache, $whois)}.to_json
+        rescue Exception => e
+                {:error => e}.to_json
+        end
   end
 end
